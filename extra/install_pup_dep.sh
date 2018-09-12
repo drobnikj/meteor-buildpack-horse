47015d56d823c09a062a21c2bcc1414d7f584a79
JakubDrobnik <drobnik.j@gmail.com>
2018-09-12 20:11:23 +0200

    extra sh for instaling puppeteer dep

--- extra/install_pup_dep.sh
+++ extra/install_pup_dep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "-----> Instaling puppeteer depedencies"
 
-apt-get update -y -q
-apt-get install -y -q xvfb libgtk2.0-0 libxtst6 libxss1 libgconf-2-4 libnss3 libasound2
+sudo apt-get update -y -q
+sudo apt-get install -y -q xvfb libgtk2.0-0 libxtst6 libxss1 libgconf-2-4 libnss3 libasound2
